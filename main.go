25da56db2353860504a88639112e41dafb6a0b35
Victor Miclotte <victor.miclotte@gmail.com>
2024-01-02 21:29:04 +0100

    Add main.go which runs an example simulation

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"prisoners-dilemma/game"
+	"prisoners-dilemma/strategies"
+)
+
+func main() {
+	numRounds := 200
+	player1 := game.NewPlayer(strategies.TitForTat{})
+	player2 := game.NewPlayer(strategies.AlwaysDefect{})
+	rules := game.Rules{
+		BothDefectGain:    1,
+		BothCooperateGain: 3,
+		WinGain:           5,
+		LossGain:          0,
+	}
+	game := game.NewGame(player1, player2, numRounds, rules)
+	game.Play()
+	fmt.Printf("Player 1 score: %d\n", player1.Score)
+	fmt.Printf("Player 2 score: %d\n", player2.Score)
+}
